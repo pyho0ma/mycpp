884c35666d9e7918151b5c5e3576f2f2b32c9e10
Aypn <80340450+aypnpillai@users.noreply.github.com>
2022-01-21 21:10:02 +0530

    first commit

---
+++ mycpp/firstcpp.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"--Program dev is @aypn--";
+    cout<<"Enter the number 1: ";
+    cout<<"Enter the number 2: ";
+    int a,b,c;
+    c=a+b;
+}
